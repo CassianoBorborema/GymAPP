17b94e4114c69332204ec31ec9926eeaef89a8f9
CassianoBorborema <ladeiacassiano@gmail.com>
2026-05-04 22:03:48 -0300

    Add Aluno, Exercise, and Instructor entity classes with validation logic

---
+++ service/domain/entities/Exercise.ts
@@ -0,0 +1,21 @@
+export type MuscleGroup = "Peito" | "Costas" | "Pernas" | "Ombros" | "Braços";
+
+export class Exercise {
+  constructor(
+    readonly id: string, //id persistente
+    public name: string,
+    public muscleGroup: MuscleGroup,
+    public videoUrl: string,
+  ) {
+    this.validate();
+  }
+
+  private validate() {
+    if (!this.name || this.name.length < 3) {
+      throw new Error("Nome do exercício muito curto.");
+    }
+    if (!this.muscleGroup) {
+      throw new Error("Grupo Muscular é obrigatório");
+    }
+  }
+}
